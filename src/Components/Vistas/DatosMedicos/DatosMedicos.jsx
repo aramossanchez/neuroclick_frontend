a52deae9628931e7dd746c5e74538ec50f532146
Armando <armandoramossanchez@gmail.com>
2021-12-24 16:22:39 +0100

    funcionalidad en datos medicos, mostrando edad, peso y estatura

--- src/Components/Vistas/DatosMedicos/DatosMedicos.jsx
+++ src/Components/Vistas/DatosMedicos/DatosMedicos.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import './DatosMedicos.scss';
+import { connect } from 'react-redux';
 
-const DatosMedicos = () =>{
+const DatosMedicos = (props) =>{
+
+    //CÁLCULO DE EDAD DEL USUARIO
+    let fechaActual = new Date();
+    let añoActual = fechaActual.getFullYear()
+    let fechaDeNacimiento = new Date(props.usuarioSeleccionado.usuario.fecha_nacimiento);
+    let añoNacimiento = fechaDeNacimiento.getFullYear();
 
     return(
         <div className='contenedor_datos_medicos contenedor_vista flex_columna_arriba_izquierda'>
             <h2>Datos médicos</h2>
             <div className="datos_medicos_usuario flex_fila_izquierda mi">
                 <label htmlFor="edad">Edad:</label>
-                <input type="email" name="edad" readOnly/>
+                <input type="email" name="edad" readOnly value={añoActual - añoNacimiento}/>
                 <label htmlFor="peso">Peso:</label>
-                <input type="email" name="peso" readOnly/>
+                <input type="email" name="peso" readOnly value={props.usuarioSeleccionado.usuario.peso}/>
                 <label htmlFor="estatura">Estatura:</label>
-                <input type="email" name="estatura" readOnly/>
+                <input type="email" name="estatura" readOnly value={props.usuarioSeleccionado.usuario.estatura}/>
             </div>
             <hr />
             <h2>Antecedentes familiares</h2>
@@ -33,4 +40,6 @@ const DatosMedicos = () =>{
     )
 }
 
-export default DatosMedicos;
+export default connect((state)=>({
+    usuarioSeleccionado: state.usuarioSeleccionado
+}))(DatosMedicos);
