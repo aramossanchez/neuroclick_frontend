fdcdd5df49d17c05bd2550179384bcf9db2db1c6
Armando <armandoramossanchez@gmail.com>
2021-12-24 13:23:03 +0100

    refactorizo usuarios para tenerlo en un componente aparte

---
+++ src/Components/Usuarios/Usuarios.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react';
+import './Usuarios.scss';
+import { connect } from 'react-redux';
+import { USUARIO } from '../../redux/types';
+import axios from 'axios';
+
+const Usuarios = (props) =>{
+
+    //HOOKS
+    const [listadoUsuarios, setListadoUsuarios] = useState([]);
+    const [listadoUsuariosFiltrados, setListadoUsuariosFiltrados] = useState([]);
+    const [mensajeError, setMensajeError] = useState("");
+
+    //AL CARGAR EL COMPONENTE, GUARDA LISTADO DE USUARIOS EN REDUX
+    useEffect(()=>{
+        traerListaUsuarios();
+    }, [])
+
+    //OBTIENE LISTADO DE USUARIOS DE LA BASE DE DATOS
+    const traerListaUsuarios = async () =>{
+
+        try {
+
+            let res = await axios.get("http://localhost:3000/usuarios");
+            setListadoUsuarios(res.data);
+            setListadoUsuariosFiltrados(res.data);
+
+        } catch (error) {
+
+            //SETEA MENSAJE DE ERROR
+            setMensajeError("Ha habido un error al intentar acceder al listado completo de usuarios");
+            //TRAS 4 SEGUNDOS, HACE DESAPARECER EL MENSAJE
+            setTimeout(() => {
+                setMensajeError("");
+            }, 4000);
+
+        }
+    }
+
+    //GUARDAR USUARIO SELECCIONADO EN REDUX
+    const elegirUsuario = (usuario, e) =>{
+        props.dispatch({type:USUARIO, payload: usuario});
+        //CAMBIAR CLASE PARA MARCAR CUAL USUARIO ESTÁ SELECCIONADO
+        let usuarios = document.getElementsByClassName('usuario_individual');
+        for (let i = 0; i < usuarios.length; i++) {
+            usuarios[i].classList.remove('usuario_seleccionado')
+        }
+        e.target.classList.add('usuario_seleccionado');
+    }
+
+    //BUSCAR USUARIO POR NOMBRE Y APELLIDOS
+    const buscarUsuario = (e) =>{
+        console.log(e.target.value)
+        let usuarios = listadoUsuarios.filter(usuario => usuario.nombre.toLowerCase().includes(e.target.value) || usuario.apellidos.toLowerCase().includes(e.target.value))
+        setListadoUsuariosFiltrados(usuarios);
+    }
+
+    return(
+        <div className="contenedor_izq bloque_principal flex_columna_muy_separado">
+            <h2>Usuarios</h2>
+            <div className="campo_formulario">
+                <input type="email" name="buscar" placeholder='Buscar usuario' onChange={(e)=>buscarUsuario(e)}/>
+            </div>
+            <div className='contenedor_usuarios flex_columna_arriba_izquierda'>
+                {listadoUsuariosFiltrados.map((usuario)=>{
+                    return(
+                        <div key={usuario.id} className='usuario_individual' onClick={(e)=>elegirUsuario(usuario, e)}>
+                            {usuario.apellidos}, {usuario.nombre} 
+                        </div>
+                    )
+                })}
+                
+            </div>
+        </div>
+    )
+}
+
+export default connect()(Usuarios);
--- src/Containers/PantallaPrincipal/PantallaPrincipal.jsx
+++ src/Containers/PantallaPrincipal/PantallaPrincipal.jsx
@@ -3,8 +3,6 @@ import './PantallaPrincipal.scss';
 import Header from '../../Components/Header/Header.jsx';
 import { connect } from 'react-redux';
 import { VISTASELECCIONADA } from '../../redux/types';
-import { USUARIO } from '../../redux/types';
-import axios from 'axios';
 
 /*IMPORTACIÓN DE VISTAS*/
 import HistoriaClinica from '../../Components/Vistas/HistoriaClinica/HistoriaClinica';
@@ -17,57 +15,9 @@ import Evolutiva from '../../Components/Vistas/Evolutiva/Evolutiva';
 import Seguimientos from '../../Components/Vistas/Seguimientos/Seguimientos';
 import Agenda from '../../Components/Vistas/Agenda/Agenda';
 import Informe from '../../Components/Vistas/Informe/Informe';
+import Usuarios from '../../Components/Usuarios/Usuarios';
 
 const PantallaPrincipal = (props) =>{
-
-    //HOOKS
-    const [listadoUsuarios, setListadoUsuarios] = useState([]);
-    const [listadoUsuariosFiltrados, setListadoUsuariosFiltrados] = useState([]);
-    const [mensajeError, setMensajeError] = useState("");
-
-    //AL CARGAR EL COMPONENTE, GUARDA LISTADO DE USUARIOS EN REDUX
-    useEffect(()=>{
-        traerListaUsuarios();
-    }, [])
-
-    //OBTIENE LISTADO DE USUARIOS DE LA BASE DE DATOS
-    const traerListaUsuarios = async () =>{
-
-        try {
-
-            let res = await axios.get("http://localhost:3000/usuarios");
-            setListadoUsuarios(res.data);
-            setListadoUsuariosFiltrados(res.data);
-
-        } catch (error) {
-
-            //SETEA MENSAJE DE ERROR
-            setMensajeError("Ha habido un error al intentar acceder al listado completo de usuarios");
-            //TRAS 4 SEGUNDOS, HACE DESAPARECER EL MENSAJE
-            setTimeout(() => {
-                setMensajeError("");
-            }, 4000);
-
-        }
-    }
-
-    //GUARDAR USUARIO SELECCIONADO EN REDUX
-    const elegirUsuario = (usuario, e) =>{
-        props.dispatch({type:USUARIO, payload: usuario});
-        //CAMBIAR CLASE PARA MARCAR CUAL USUARIO ESTÁ SELECCIONADO
-        let usuarios = document.getElementsByClassName('usuario_individual');
-        for (let i = 0; i < usuarios.length; i++) {
-            usuarios[i].classList.remove('usuario_seleccionado')
-        }
-        e.target.classList.add('usuario_seleccionado');
-    }
-
-    //BUSCAR USUARIO POR NOMBRE Y APELLIDOS
-    const buscarUsuario = (e) =>{
-        console.log(e.target.value)
-        let usuarios = listadoUsuarios.filter(usuario => usuario.nombre.toLowerCase().includes(e.target.value) || usuario.apellidos.toLowerCase().includes(e.target.value))
-        setListadoUsuariosFiltrados(usuarios);
-    }
 
     //SELECCIONA QUE VISTA ESTARÁ ACTIVA EN LA APLICACIÓN
     let vistas = document.getElementsByClassName('vista');
@@ -85,22 +35,7 @@ const PantallaPrincipal = (props) =>{
         <div className='contenedor_principal contenedor flex_columna_muy_separado'>
             <Header/>
             <div className="bloques_principal flex_fila_mas_separado">
-                <div className="contenedor_izq bloque_principal flex_columna_muy_separado">
-                    <h2>Usuarios</h2>
-                    <div className="campo_formulario">
-                        <input type="email" name="buscar" placeholder='Buscar usuario' onChange={(e)=>buscarUsuario(e)}/>
-                    </div>
-                    <div className='contenedor_usuarios flex_columna_arriba_izquierda'>
-                        {listadoUsuariosFiltrados.map((usuario)=>{
-                            return(
-                                <div key={usuario.id} className='usuario_individual' onClick={(e)=>elegirUsuario(usuario, e)}>
-                                    {usuario.apellidos}, {usuario.nombre} 
-                                </div>
-                            )
-                        })}
-                        
-                    </div>
-                </div>
+                <Usuarios/>
                 <div className="contenedor_der bloque_principal">
                     <div className="vistas flex_fila_izquierda">
                         <div className="vista vista_activa" onClick={(e)=>activarVista(e, 'historiaclinica')}>Historia Clínica</div>
